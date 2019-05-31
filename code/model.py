b5bb17b06dd9fd87dd1712b0bc5f44debd6e8051
Tianxin <ttx12345677@gmail.com>
2019-05-30 23:25:52 -0700

    resize image and mask

--- code/model.py
+++ code/model.py
@@ -6,11 +6,12 @@ from torchsummary import summary
 from dataloader import VideoSegmentationDataset
 # Device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-torch.cuda.empty_cache() 
+#device = torch.device('cpu')
+#torch.cuda.empty_cache() 
 # Hyper parameters
 num_epochs = 5
 num_classes = 35
-batch_size = 100
+batch_size = 1
 learning_rate = 0.003
 
 ## MNIST dataset
@@ -40,54 +41,54 @@ class FCN(nn.Module):
         super(FCN, self).__init__()
         
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 48, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(48),
+            nn.Conv2d(3, 24, kernel_size=1, stride=1, padding=1),
+            nn.BatchNorm2d(24),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2))
         
         self.layer2 = nn.Sequential(
-            nn.Conv2d(48, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(24, 36, kernel_size=1, stride=1, padding=1),
+            nn.BatchNorm2d(36),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2))
         
         self.layer3 = nn.Sequential(
-            nn.Conv2d(128, 192, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(36, 64, kernel_size=1, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2))
         
         self.layer4 = nn.Sequential(
-            nn.Conv2d(192, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(64, 96, kernel_size=1, stride=1, padding=1),
+            nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2))
         
         self.layer5 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(96, 128, kernel_size=1, stride=1, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2))
         
         self.layer6 = nn.Sequential(
-            nn.Conv2d(256, 448, kernel_size=7, stride=1, padding=1),
-            nn.BatchNorm2d(512),
+            nn.Conv2d(128, 192, kernel_size=1, stride=1, padding=1),
+            nn.BatchNorm2d(192),
             nn.ReLU())
         
         self.layer7 = nn.Sequential(
-            nn.Conv2d(378, 378, kernel_size=1, stride=1, padding=1),
-            nn.BatchNorm2d(378),
+            nn.Conv2d(192, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU())
 
 #        self.fc = nn.Linear(7*7*32, num_classes)
         self.score_fr = nn.Sequential(
-                nn.Conv2d(378,35,kernel_size=1,padding=0))
+                nn.Conv2d(256,35,kernel_size=1,padding=0))
         
         self.upsampling = nn.Sequential(
-                nn.ConvTranspose2d(35,35,kernel_size=32,stride=64))
+                nn.ConvTranspose2d(35,35,kernel_size=72,stride=28))
         
     def forward(self, x):
-        out = self.layer1(x)
+        out = self.layer1(x.view(-1,3,2710,3384))
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
@@ -101,7 +102,7 @@ class FCN(nn.Module):
         return out
 
 model = FCN().to(device)
-print(summary(model,(3,1024,1024)))
+print(summary(model,(3,2710,3384)))
 
 
 # Loss and optimizer
@@ -111,12 +112,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 # Train the model
 total_step = len(train_loader)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
-        
+    for i, data in enumerate(train_loader):
+#         print(data)
+        images = data['image'].type('torch.DoubleTensor').to(device)
+        labels = data['label'].type('torch.DoubleTensor').to(device)
+        print(1)
         # Forward pass
-        outputs = model(images)
+        outputs = model(images.double())
         loss = criterion(outputs, labels)
         
         # Backward and optimize
@@ -124,7 +126,7 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         
-        if (i+1) % 100 == 0:
+        if (i+1) % 10 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
--- code/utils.py
+++ code/utils.py
@@ -3,23 +3,9 @@ from skimage.io import imread,imshow,show
 import numpy as np
 import os
 from matplotlib import pyplot as plt
+from skimage.transform import resize
+import scipy
 
-#sampledTrainFolderPath = '../train_color_sample/'
-#sampledLabelFolderPath = '../train_label_sample/'
-#
-#train_images = np.array(sorted(os.listdir(sampledTrainFolderPath)))
-#train_labels = np.array(sorted(os.listdir(sampledLabelFolderPath)))
-#
-#
-#trainPic = imread(sampledTrainFolderPath+train_images[0]) 
-#labelPic = imread(sampledLabelFolderPath+train_labels[0])
-#imgHeight = labelPic.shape[0]
-#imgWidth = labelPic.shape[1]
-#
-#imshow(trainPic)
-#show()
-#imshow(labelPic)
-#show()
 
 def CreateIndexMap():
     # Construct a dictionary
@@ -66,12 +52,11 @@ def CreateIndexMap():
         index = index+1
         
     numOfClasses = str(len(labelmap))
-    print("The output has " + str(len(labelmap)) + " channels")
+#    print("The output has " + str(len(labelmap)) + " channels")
     return indexMap
 
 # The output would be a (35,2710,3384) vector
-def ConvertLabelImage(imgArr):
-    indexMap = CreateIndexMap()
+def ConvertLabelImage(imgArr,indexMap):
     numOfClasses = len(indexMap)
     imgArray = np.array(imgArr/1000).astype(int)
     output = np.zeros((numOfClasses,imgArray.shape[0],imgArray.shape[1]))
@@ -80,5 +65,65 @@ def ConvertLabelImage(imgArr):
     return output
 
 
+def ResizeImage(imgArr, height = 1024, width = 1024):
+    originalHeight = imgArr.shape[0]
+    originalWidth = imgArr.shape[1]
+    ratio = width/max(originalWidth,originalHeight)
+    imgArr = resize(imgArr,(round(ratio*originalHeight),round(ratio*originalWidth)),preserve_range = True)
+    h,w = imgArr.shape[:2]
+    print(height,width)
+    max_dim = max(height,width)
+    top_pad = (max_dim - h) // 2
+    bottom_pad = max_dim - h - top_pad
+    left_pad = (max_dim - w) // 2
+    right_pad = max_dim - w - left_pad
+    padding = [(top_pad, bottom_pad), (left_pad, right_pad), (0, 0)]
+    imgArr = np.pad(imgArr, padding, mode='constant', constant_values=0)
+    window = (top_pad, left_pad, h + top_pad, w + left_pad)
+    return imgArr.astype('uint8'),ratio,window,padding
+
+def ResizeMaskPiece(mask, scale, padding):
+    mask = scipy.ndimage.zoom(mask, zoom=[scale, scale], order=0)
+    mask = np.pad(mask, padding[0:2], mode='constant', constant_values=0)
+    return mask
+
+def Resize(imgArr,labelArr, height = 1024, width = 1024):
+    indexMap = CreateIndexMap()
+    resizedImg,scale,window,padding = ResizeImage(imgArr)
+    labelMask = ConvertLabelImage(labelArr,indexMap)
+    resizedMask = np.zeros((len(indexMap),height,width))
+    
+    for i in range(len(indexMap)):
+        resizedMask[i] = ResizeMaskPiece(labelMask[i],scale,padding)
+        
+    return resizedImg,resizedMask
+    
+    
+    
+    
+if __name__ == '__main__':
+    
+    sampledTrainFolderPath = '../train_color_sample/'
+    sampledLabelFolderPath = '../train_label_sample/'
+    
+    train_images = np.array(sorted(os.listdir(sampledTrainFolderPath)))
+    train_labels = np.array(sorted(os.listdir(sampledLabelFolderPath)))
+    
+    
+    trainPic = imread(sampledTrainFolderPath+train_images[0]) 
+    labelPic = imread(sampledLabelFolderPath+train_labels[0])
+    imgHeight = labelPic.shape[0]
+    imgWidth = labelPic.shape[1]
+    
+    imshow(trainPic)
+    show()
+    imshow(labelPic)
+    show()
+    
+#    newPic,scale,window,padding = ResizeImage(trainPic)
+#    mask = ResizeMask(ConvertLabelImage(labelPic,indexMap),scale,padding)
+#    imshow(np.round(newPic))
+#    imshow(mask)
+    resizedImg,resizedMask = Resize(trainPic,labelPic)
 
 
